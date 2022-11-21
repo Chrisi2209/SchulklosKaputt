a4cba5e9ee9d783afdfa9261da08a681b6801ad6
Chrisi2209 <christopher.rodax@gmail.com>
2022-11-21 22:22:09 +0100

    Big progress

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 from app import app
 from app.forms import NeuesKloForm, KloLöschenForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
  
 kaputte_klos = []
 
 @app.route("/") 
 @app.route("/index") 
 def index(): 
-    return render_template("index.html", anzahl=len(kaputte_klos))
+    return render_template("index.html", title="HTL-Mödling kaputte klos", anzahl=len(kaputte_klos))
 
 @app.route("/kloansicht", methods=["GET", "POST"])
 def kloansicht():
@@ -17,9 +17,9 @@ def kloansicht():
                         if form.pissoir.data else 
                          f"{form.gebäudenummer.data:02d}{form.stocknummer.data:02d}{form.zimmernummer.data:02d}:Klo:{form.klonummer.data:02d}")
         kaputte_klos.append(composed_name)
-        return redirect('/index')
+        return redirect(url_for("index"))
 
-    return render_template("kloansicht.html", kaputte_klos=kaputte_klos, form=form)
+    return render_template("kloansicht.html", title="HTL-Mödling Kloansicht", kaputte_klos=kaputte_klos, form=form)
 
 @app.route("/kloansicht/<klo>", methods=["GET", "POST"])
 def kloansicht_klo(klo):
@@ -27,9 +27,9 @@ def kloansicht_klo(klo):
     if form.validate_on_submit(): 
         flash(f'deleted: {klo}') 
         kaputte_klos.remove(klo)
-        return redirect('/index')
+        return redirect(url_for("index"))
 
     if klo in kaputte_klos:
-        return render_template("klobearbeitung.html", klo=klo, form=form)
+        return render_template("klobearbeitung.html", title=f"HTL-Mödling {klo} bearbeiten", klo=klo, form=form)
     else:
-        return redirect("/kloansicht")
+        return redirect(url_for("kloansicht"))
