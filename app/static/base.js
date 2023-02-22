d8765e510c6cdea1cdfeb991b6d0510198b37ae6
Chrisi2209 <christopher.rodax@gmail.com>
2023-02-22 10:38:46 +0100

    started on dropdown_icon for navbar

---
+++ app/static/base.js
@@ -0,0 +1,6 @@
+var navbar = document.getElementsByClassName("navbar")[0];
+var dropdown_container = document.getElementsByClassName("dropdownicon-container")[0];
+
+function openNavbar() {
+    if (dropdown_container.className == "") { }
+}
