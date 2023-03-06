1e7b2d4e7d7797d35ac8a65deee057fa132374d7
Chrisi2209 <christopher.rodax@gmail.com>
2023-03-06 20:08:44 +0100

    dropdownmenu done

--- app/static/base.js
+++ app/static/base.js
@@ -1,6 +1,47 @@
 var navbar = document.getElementsByClassName("navbar")[0];
 var dropdown_container = document.getElementsByClassName("dropdownicon-container")[0];
 
-function openNavbar() {
-    if (dropdown_container.className == "") { }
+function mouseLeft() {
+    console.log("mouse left")
+}
+
+function dropdownEndEvent() {
+    console.log("mouse left")
+    dropdown_container.removeEventListener("animationend", dropdownEndEvent);
+    dropdown_container.removeEventListener('mouseleave', dropupEndEvent)
+
+    navbar.classList.remove("drop-animation");
+    dropdown_container.classList.remove("drop-animation");
+}
+
+function dropupEndEvent() {
+    dropdown_container.removeEventListener("animationend", dropupEndEvent);
+
+    navbar.classList.remove("dropup-animation");
+    dropdown_container.classList.remove("dropup-animation");
+}
+
+function toggleNavbar() {
+    navbar.removeEventListener('mouseleave', toggleNavbar)
+    if (!dropdown_container.classList.contains("dropped")) {
+        dropdown_container.classList.add("dropped");
+        navbar.classList.add("dropped")
+
+        dropdown_container.classList.remove("dropdownicon-hoverdrop");  // no more hover feedback
+
+        navbar.classList.add("drop-animation");
+        dropdown_container.classList.add("drop-animation");
+        dropdown_container.addEventListener('animationend', dropdownEndEvent)
+        navbar.addEventListener('mouseleave', toggleNavbar)
+    }
+    else if (dropdown_container.classList.contains("dropped")) {
+        navbar.classList.remove("dropped");
+        dropdown_container.classList.remove("dropped");
+
+        dropdown_container.classList.add("dropdownicon-hoverdrop");  // avtivate hover feedback
+
+        navbar.classList.add("dropup-animation");
+        dropdown_container.classList.add("dropup-animation");
+        dropdown_container.addEventListener('animationend', dropupEndEvent)
+    }
 }
